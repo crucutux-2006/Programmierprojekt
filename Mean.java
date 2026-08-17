1cc38d5cb40a1bf7ff6379d060d764c278e21e39
Crucutux <Crucutux@gmail.com>
2026-08-17 13:46:09 +0200

    Create Mean.java

---
+++ Mean.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Mean{
+    public static void main(String[]args){
+
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  //n ist anzahl Zahlen
+
+        int i=0;
+        int [] arr = new int[i];
+
+        //Math.floor();
+
+
+    }
+}
