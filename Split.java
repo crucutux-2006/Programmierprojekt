3ff8ce07fce2936f7f5e36c2c928e63341689db6
Crucutux <Crucutux@gmail.com>
2026-08-17 13:22:42 +0200

    Update Split.java

--- Split.java
+++ Split.java
@@ -8,6 +8,46 @@ public class Split{
 
         System.out.println("Enter Number (digits must be even!)");
         String s = sc.nextLine();
+        int half = 0;
+        if(sc.hasNextLine()){
+            int length = s.length();
+            half = length / 2;
+        } else {System.out.println("Digits are not even");}
+        
+        System.out.println(half);
+
+
+    }
+}import java.util.Scanner;
+public class Split{
+    public static void main(String[] args) {
+        
+        //Idee: Arraylänge und dann in 2 teilen und erst das erste dann das zweite array ausgeben
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Number (digits must be even!)");
+        String s = sc.nextLine();
+        int half = 0;
+        if(sc.hasNextLine()){
+            int length = s.length();
+            half = length / 2;
+        } else {System.out.println("Digits are not even");}
+        
+        System.out.println(half);
+
+
+    }
+}import java.util.Scanner;
+public class Split{
+    public static void main(String[] args) {
+        
+        //Idee: Arraylänge und dann in 2 teilen und erst das erste dann das zweite array ausgeben
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Number (digits must be even!)");
+        String s = sc.nextLine();
         if(sc.hasNextLine()){
             int length = s.length();
             int half = length / 2;
