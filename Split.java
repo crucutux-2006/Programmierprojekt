35c5aa865d5d0486e0f6e35ec614d1ae677f6bf8
Crucutux <Crucutux@gmail.com>
2026-08-17 13:32:21 +0200

    Update Split.java

--- Split.java
+++ Split.java
@@ -5,7 +5,6 @@ public class Split{
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter Number (digits must be even!)");
         String s = sc.nextLine();
         
             int length = s.length();
@@ -14,67 +13,9 @@ public class Split{
         String part1 = s.substring(0,half);
         String part2 = s.substring(half);
         
-        System.out.println(part1 + "\n"+ part2);
+        System.out.println(part1 + " " + part2);
 
         sc.close();
-
-    }
-}import java.util.Scanner;
-public class Split{
-    public static void main(String[] args) {
-        
-        //Idee: Arraylänge und dann in 2 teilen und erst das erste dann das zweite array ausgeben
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Number (digits must be even!)");
-        String s = sc.nextLine();
-        int half = 0;
-        if(sc.hasNextLine()){
-            int length = s.length();
-            half = length / 2;
-        } else {System.out.println("Digits are not even");}
-        
-        System.out.println(half);
-
-
-    }
-}import java.util.Scanner;
-public class Split{
-    public static void main(String[] args) {
-        
-        //Idee: Arraylänge und dann in 2 teilen und erst das erste dann das zweite array ausgeben
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Number (digits must be even!)");
-        String s = sc.nextLine();
-        int half = 0;
-        if(sc.hasNextLine()){
-            int length = s.length();
-            half = length / 2;
-        } else {System.out.println("Digits are not even");}
-        
-        System.out.println(half);
-
-
-    }
-}import java.util.Scanner;
-public class Split{
-    public static void main(String[] args) {
-        
-        //Idee: Arraylänge und dann in 2 teilen und erst das erste dann das zweite array ausgeben
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter Number (digits must be even!)");
-        String s = sc.nextLine();
-        if(sc.hasNextLine()){
-            int length = s.length();
-            int half = length / 2;
-        } else {System.out.println("Digits are not even");}
-        System.out.println(half);
-
 
     }
 }
