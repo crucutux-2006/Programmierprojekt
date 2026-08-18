c5dd2f80867fce8c5dea753518d3a29b6271c524
Crucutux <Crucutux@gmail.com>
2026-08-18 09:53:36 +0200

    Update NiceSet.java

--- NiceSet.java
+++ NiceSet.java
@@ -20,6 +20,47 @@ public class NiceSet {
         if(sc.hasNextInt()){
             n = sc.nextInt();}
 
+            int i = 0;
+            //Eingabewerte Array
+           int[] arr = new int [n];  
+           //System.out.println("Enter integers in the array");
+           for (i = 0; i < n; i++){
+            if (sc.hasNext()) {
+                arr[i] = sc.nextInt();
+            }   
+           }
+
+        //Berechnung 
+        int j = 0;
+        for(j = 0;j < s;j++){
+            if(arr[i] < s)
+            Math.abs(arr[i] < s);
+        }
+
+        sc.close();
+    }
+}import java.util.Scanner;
+
+public class NiceSet {
+    public static void main(String[]args){
+        
+       
+        
+
+        Scanner sc = new Scanner(System.in);
+        
+        //Initialisierung S
+        int s = 0;
+        if(sc.hasNext()){
+            s = sc.nextInt();
+        }
+
+        //Initialisierung Arraygröße
+        System.out.println("Array Size:");
+        int n = 0;                  //n ist Userinput
+        if(sc.hasNextInt()){
+            n = sc.nextInt();}
+
             //Eingabewerte Array
            int[] arr = new int [n];  
            //System.out.println("Enter integers in the array");
